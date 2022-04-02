ffccd821af3cf642a6c8f95cd163901a0bd96736
Erik Rantanen <erik.rantanen@hotmail.fi>
2022-04-02 17:21:28 +0300

    Add some base data

--- src/app/services/criminalRecord/criminal-record.service.ts
+++ src/app/services/criminalRecord/criminal-record.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { CriminalRecord } from 'src/app/model/criminalRecord.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CriminalRecordService {
 
-  constructor() { }
+  criminalRecordMap = new Map();
+
+  constructor() {
+    const personOne = new CriminalRecord(1, 1, 'Cooking crystal meth');
+    this.criminalRecordMap.set(personOne.id, personOne);
+  }
 }
