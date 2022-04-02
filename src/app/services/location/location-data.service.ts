d58626ea5cb0f9969e323e7a688cf8e3b651b324
Erik Rantanen <erik.rantanen@hotmail.fi>
2022-04-02 17:25:15 +0300

    Add some base data

--- src/app/services/location/location-data.service.ts
+++ src/app/services/location/location-data.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { LocationData } from 'src/app/model/locationData.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationDataService {
 
-  constructor() { }
+  locationDataList: Array<LocationData> = [];
+
+  constructor() {
+    this.locationDataList.push(new LocationData(1, 1, 123, 456));
+  }
 }
