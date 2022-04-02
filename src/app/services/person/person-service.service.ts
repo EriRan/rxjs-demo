2abe1802cae4ef88f28e52ba625e030fc0818195
Erik Rantanen <erik.rantanen@hotmail.fi>
2022-04-02 17:16:49 +0300

    Add some base data

--- src/app/services/person/person-service.service.ts
+++ src/app/services/person/person-service.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Person } from 'src/app/model/person.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonServiceService {
 
-  constructor() { }
+  personMap = new Map();
+
+  constructor() {
+    const personOne = new Person(1, 'Walter White');
+    this.personMap.set(personOne.id, personOne);
+  }
 }
