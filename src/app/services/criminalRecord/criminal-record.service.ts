0ec8f9d98884fc42c581a33fd3de0769d26ffe23
Erik Rantanen <erik.rantanen@hotmail.fi>
2022-04-02 17:41:39 +0300

    Add get methods
    These are simulating API calls with delay

--- src/app/services/criminalRecord/criminal-record.service.ts
+++ src/app/services/criminalRecord/criminal-record.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { delay, Observable, of } from 'rxjs';
 import { CriminalRecord } from 'src/app/model/criminalRecord.model';
 
 @Injectable({
@@ -6,9 +7,21 @@ import { CriminalRecord } from 'src/app/model/criminalRecord.model';
 })
 export class CriminalRecordService {
 
-  criminalRecordList: Array<CriminalRecord> = [];
+  private criminalRecordList: Array<CriminalRecord> = [];
 
   constructor() {
     this.criminalRecordList.push(new CriminalRecord(1, 1, 'Cooking crystal meth'));
+  }
+
+  /**
+   * Simulates an API call to a get endpoint. Returns an observable that is returned after 1 second of delay
+   * @param personId 
+   * @returns 
+   */
+  getByPersonId(personId: number): Observable<CriminalRecord | undefined> {
+    const getObservable = of(this.criminalRecordList.find(criminalRecord => criminalRecord.personId === personId));
+    return getObservable.pipe(
+      delay(1000)
+    );
   }
 }
--- src/app/services/location/location-data.service.ts
+++ src/app/services/location/location-data.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of, delay } from 'rxjs';
 import { LocationData } from 'src/app/model/locationData.model';
 
 @Injectable({
@@ -10,5 +11,17 @@ export class LocationDataService {
 
   constructor() {
     this.locationDataList.push(new LocationData(1, 1, 123, 456));
+  }
+
+  /**
+   * Simulates an API call to a get endpoint. Returns an observable that is returned after 1 second of delay
+   * @param personId 
+   * @returns 
+   */
+   getByPersonId(personId: number): Observable<LocationData | undefined> {
+    const getObservable = of(this.locationDataList.find(locationData => locationData.personId === personId));
+    return getObservable.pipe(
+      delay(1000)
+    );
   }
 }
