7c1c7dc1bd092993014d01daa5e6a1f384fb862d
EriRan <erik.rantanen@hotmail.fi>
2022-04-03 18:40:00 +0300

    Add simple person load
    Where to handle these observables? How should I display loading?

--- src/app/display-component/display-component.component.ts
+++ src/app/display-component/display-component.component.ts
@@ -1,22 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { filter, first, map, of } from 'rxjs';
+import { Person } from '../model/person.model';
+import { PersonService } from '../services/person/person.service';
 
 @Component({
   selector: 'app-display-component',
   templateUrl: './display-component.component.html',
-  styleUrls: ['./display-component.component.css']
+  styleUrls: ['./display-component.component.css'],
 })
 export class DisplayComponentComponent implements OnInit {
 
-  constructor() { }
+  person?: Person;
+  callDone: boolean = false;
 
-  ngOnInit(): void {
-  }
+  constructor(private personService: PersonService) {}
+
+  ngOnInit(): void {}
 
   /**
    * HTML
    */
-  displayPerson(): void {
-    console.log('Display');
+  displayPerson(id: number): void {
+    this.person = undefined;
+    this.callDone = true;
+    this.personService
+      .get(id)
+      .subscribe((responsePerson) => {
+        if (!responsePerson) {
+          this.person = new Person(-1, 'NOT FOUND');
+          return;
+        }
+        this.person = responsePerson;
+      });
   }
-
 }
