202561d68a676987a05abd816a36396becfc90f5
EriRan <erik.rantanen@hotmail.fi>
2022-04-06 18:39:18 +0300

    Remove criminal record component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DisplayComponent as DisplayComponent } from './display-component/display.component';
 import { PersonComponent } from './component/person-component/person.component';
-import { CriminalRecordComponent } from './component/criminal-record/criminal-record.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DisplayComponent,
-    PersonComponent,
-    CriminalRecordComponent
+    PersonComponent
   ],
   imports: [
     BrowserModule
